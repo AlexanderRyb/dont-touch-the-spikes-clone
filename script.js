a388e2fc183996821baac1e73b49b4c393406b8e
Alexander <AlexanderRy@protonmail.com>
2022-01-24 16:02:55 +0200

    added  left obstacles and collision detection

--- script.js
+++ script.js
@@ -20,13 +20,25 @@ let vy = 5; //vertical velocity
 
 let ay = -0.25; //gravity
 let ax = 0; //air resistance
-let score = 0
-document.getElementById("score").innerHTML = score
+let score = 0;
 
-document.getElementsByClassName("score").innerHTML="fddfadd"
+document.getElementById("score").innerHTML = score;
 
+document.getElementsByClassName("score").innerHTML = "fddfadd";
 
 let interval;
+function newGame() {
+  y = 200;
+  x = 150;
+  character.style.bottom = y + "px";
+  character.style.left = x + "px";
+  direction = jumpingRight;
+
+  console.log("you lost! Your score is " + score);
+  score = 0;
+
+  clearInterval(interval);
+}
 function jump() {
   vx = direction;
   vy = 5;
@@ -40,42 +52,117 @@ function jump() {
     character.style.bottom = y + "px";
     character.style.left = x + "px";
 
-    if (x > 380) {
-      vx = -vx
+    if (x > 280) {
+      vx = -vx;
       direction = jumpingLeft;
-      score++
-    
-document.getElementById("score").innerHTML = score
-      
+      score++;
 
-      
-
+      document.getElementById("score").innerHTML = score;
     }
     if (x < 0) {
-      vx = -vx
+      vx = -vx;
       direction = jumpingRight;
 
-      score++
-      document.getElementById("score").innerHTML = score
-
-
-
-
+      score++;
+      document.getElementById("score").innerHTML = score;
     }
 
-    if (y <0 || y>370) {
-      y = 200;
-      x = 200;
-      character.style.bottom = y + "px";
-      character.style.left = x + "px";
-      direction = jumpingRight;
-
-      console.log("you lost! Your score is "+ score);
-      score = 0;
-
-      clearInterval(interval)
+    if (y < 0 || y > 370) {
+      newGame();
     }
-    //barrier collision detection
-    
+    //check if character div reached left side of the screen
+    if (x < 0) {
+      console.log("left side touched" + y);
+      //check  where exactly did it reach it on y axis
+      if (y > 360) {
+        //check if an obstacle exists in that place
+        let leftBarrierContainer1 = document.querySelector(
+          ".left-barrier-container-1"
+        );
+        if (leftBarrierContainer1.firstChild) {
+          newGame();
+        }
+      }
+      //is there any way not to repeat this code so many times?
+      else if (y > 320 && y < 360) {
+        let leftBarrierContainer2 = document.querySelector(
+          ".left-barrier-container-2"
+        );
+        if (leftBarrierContainer2.firstChild) {
+          newGame();
+        }
+      }
+      else if (y > 280 && y < 320) {
+        let leftBarrierContainer3 = document.querySelector(
+          ".left-barrier-container-3"
+        );
+        if (leftBarrierContainer3.firstChild) {
+          newGame();
+        }
+      }
+      else if (y>240&&y<280){
+        let leftBarrierContainer4 = document.querySelector(
+          ".left-barrier-container-4"
+        );
+        if (leftBarrierContainer4.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>200&&y<240){
+        let leftBarrierContainer5 = document.querySelector(
+          ".left-barrier-container-5"
+        );
+        if (leftBarrierContainer5.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>160&&y<200){
+        let leftBarrierContainer6 = document.querySelector(
+          ".left-barrier-container-6"
+        );
+        if (leftBarrierContainer6.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>120&&y<160){
+        let leftBarrierContainer7 = document.querySelector(
+          ".left-barrier-container-7"
+        );
+        if (leftBarrierContainer7.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>80&&y<120){
+        let leftBarrierContainer8 = document.querySelector(
+          ".left-barrier-container-8"
+        );
+        if (leftBarrierContainer8.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>40&&y<80){
+        let leftBarrierContainer9 = document.querySelector(
+          ".left-barrier-container-9"
+        );
+        if (leftBarrierContainer9.firstChild) {
+      newGame()   
+        }
+
+      }
+      else if (y>0&&y<40){
+        let leftBarrierContainer10 = document.querySelector(
+          ".left-barrier-container-10"
+        );
+        if (leftBarrierContainer10.firstChild) {
+      newGame()   
+        }
+
+      }
+    }
   }, 15);
 }
